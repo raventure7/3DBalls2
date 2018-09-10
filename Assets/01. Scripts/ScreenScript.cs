f573af690bef854120dfbc1e3742318cf38f472b
lee <raventure7@gmail.com>
2018-09-10 23:10:58 +0900

    앱 배포 버전 v1.0
    수고했습니당.

---
+++ Assets/01. Scripts/ScreenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenScript : MonoBehaviour {
+    private void Awake()
+    {
+        //Screen.SetResolution(720, 1280, false);
+        //Screen.SetResolution(450, 800, false);
+        //Screen.SetResolution(1440, 2560, false);
+        Screen.SetResolution(Screen.width, (Screen.width * 16) / 10, true);
+    }
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
